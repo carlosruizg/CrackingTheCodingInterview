c6bd7f76de2711415380717ccde054bcc15b00d1
Carlos Ruiz <carlos@joinbelle.com>
2017-04-01 21:00:11 +0100

    First exercise

---
+++ Chapter1/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is Word Unique?");
+            var input = Console.ReadLine();
+            while(input != "quit"){
+            	Console.WriteLine("Enter word");
+            	Console.WriteLine($"Is Unique: {IsUnique(input)}");
+            	input = Console.ReadLine();
+            }
+        }
+
+        static private bool IsUnique(string s){
+        	if(s.Length > 256)
+        		return false;
+        		
+        	var existCharacter = new bool[256];
+        	foreach(var character in s.ToCharArray()){
+        		if(existCharacter[(int)character])
+        			return false;
+
+        		existCharacter[(int)character] = true;
+        	}
+        	return true;
+        }
+    }
+}
