8e8a56ae1e8d3a607c77ae52ca4ca0f1e08bf408
Carlos Ruiz <carlos@joinbelle.com>
2017-04-01 22:20:22 +0100

    Replace Whitespace using Linq

---
+++ Chapter1/Exercise4/ReplaceWhitespace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ReplaceWhitespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Replace Whitespaces!");
+            Console.WriteLine("Enter Word");
+            var input = Console.ReadLine();
+            while(input != "quit")
+            {
+            	Console.WriteLine(ReplaceWhitespace(input));
+            	input = Console.ReadLine();
+            }
+        }
+
+        static string ReplaceWhitespace(string s)
+        {
+        	return string.Join("%20", s.Trim().Split()).ToString();
+        }
+    }
+}
