b7ff52a68300d76dc189ba0c9704f335802c066b
Carlos Ruiz <carlos@joinbelle.com>
2017-04-01 21:29:41 +0100

    Exercise 1: Adds unique bitwise operation

--- Chapter1/Exercise1/Program.cs
+++ Chapter1/Exercise1/Program.cs
@@ -10,21 +10,37 @@ namespace Exercise1
             var input = Console.ReadLine();
             while(input != "quit"){
             	Console.WriteLine("Enter word");
-            	Console.WriteLine($"Is Unique: {IsUnique(input)}");
+            	Console.WriteLine($"Is Unique: {UsUniqueBinary(input)}");
             	input = Console.ReadLine();
             }
         }
 
-        static private bool IsUnique(string s){
+        static private bool IsUnique(string s)
+        {
         	if(s.Length > 256)
         		return false;
-        		
+
         	var existCharacter = new bool[256];
         	foreach(var character in s.ToCharArray()){
         		if(existCharacter[(int)character])
         			return false;
 
         		existCharacter[(int)character] = true;
+        	}
+        	return true;
+        }
+
+        // Supossing only lowercase letters
+        static private bool UsUniqueBinary(string s)
+        {
+        	int exist = 0;
+        	foreach( var c in s.ToCharArray())
+        	{
+        		if((exist & (1 << (int)c)) > 0)
+        		{
+        			return false;
+        		}
+        		exist |= 1 << (int)c;
         	}
         	return true;
         }
